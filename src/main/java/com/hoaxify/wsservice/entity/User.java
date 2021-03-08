52528f05fa0a28aa1586043680d55b05be0137ef
emrekar <aliemrekaradag1@gmail.com>
2021-03-09 00:13:13 +0300

    minor

--- src/main/java/com/hoaxify/wsservice/entity/User.java
+++ src/main/java/com/hoaxify/wsservice/entity/User.java
@@ -1,7 +1,9 @@
 package com.hoaxify.wsservice.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
@@ -9,7 +11,10 @@ import javax.persistence.*;
 @Entity
 @Builder
 @Table(name = "USER")
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,7 +28,7 @@ public class User {
     @Column(name = "PASSWORD", length = 100)
     private String password;
 
-
-
-
 }
+
+
+
